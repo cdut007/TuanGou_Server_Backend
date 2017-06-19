9a2239b5b96a71b92db9bf8e622a17e4024d5135
Mike.zk <rock_or_bust@qq.com>
2017-06-19 09:55:08 +0800

    ..

--- iuser/models.py
+++ iuser/models.py
@@ -11,7 +11,7 @@ class UserProfile(models.Model):
     nickname = models.CharField(max_length=64, verbose_name=u'昵称')
     openid = models.CharField(max_length=256, verbose_name='openId')
     unionid = models.CharField(max_length=256, verbose_name='unionid')
-    sex = models.CharField(max_length=10, choices=(('male', u'男'), ('female', u'女')), default='male', verbose_name='性别')
+    sex = models.CharField(max_length=2, choices=(('1', u'男'), ('2', u'女')), default='1', verbose_name='性别')
     province = models.CharField(max_length=32, verbose_name='省份')
     city = models.CharField(max_length=32, verbose_name='城市')
     country = models.CharField(max_length=32, verbose_name='国家')
