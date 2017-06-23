0c56d1d25964f648788881545dc15e00e160de06
Mike.zk <rock_or_bust@qq.com>
2017-06-23 17:47:38 +0800

    add cross domain ignore

--- ilinkgo/settings.py
+++ ilinkgo/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'rest_framework',
     # 'rest_framework_jwt',
     'market',
-    'iuser'
+    'iuser',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -146,3 +148,34 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'images')
 ]
 IMAGES_ROOT = 'images'
+
+#add  cross-domain ignore
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    '*'
+)
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW',
+)
+
+CORS_ALLOW_HEADERS = (
+    'XMLHttpRequest',
+    'X_FILENAME',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'Pragma',
+)
