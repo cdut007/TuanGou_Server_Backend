fcbb0fe5b8d62b38dd4ab22ae6c5f29543635a41
Mike.zk <rock_or_bust@qq.com>
2017-08-01 14:10:08 +0800

    change admin order change_list page

--- iuser/admin.py
+++ iuser/admin.py
@@ -36,10 +36,9 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 class AgentOrderAdmin(admin.ModelAdmin):
     list_display = ('id','user', 'group_buy', 'add_time')
-    search_fields = ('user',)
-    actions_selection_counter = True
-    date_hierarchy = 'add_time'
+    search_fields = ('user__nickname', 'group_buy__title')
     actions = None
+
 
     def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
         render_change_form = super(AgentOrderAdmin, self).changeform_view(request, object_id, form_url, extra_context)
@@ -74,7 +73,7 @@ class AgentOrderAdmin(admin.ModelAdmin):
             cursor.execute(sql3)
             group_buy_info = dict_fetch_all(cursor)
 
-            render_change_form.context_data['group_buy_info'] = group_buy_info[0]
+            render_change_form.context_data['group_buy_info'] = group_buy_info[0] or []
 
         return render_change_form
 
