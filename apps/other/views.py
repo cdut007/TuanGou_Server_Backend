9be9c1aebe588fe2022ec7d58e9978dc0e595495
Mike.zk <rock_or_bust@qq.com>
2017-12-12 18:22:08 +0800

    .

--- apps/other/views.py
+++ apps/other/views.py
@@ -109,7 +109,7 @@ class SendOrderInfoView(APIView):
             return Response(format_body(1, 'Success', ''))
 
         if request.data['send_type'] == 'weixin':
-            excel_url = 'https://docs.google.com/gview?embedded=true&url=' + 'www.ailinkgo.com/admin/excels/' + send_record[0].excel_path
+            excel_url = 'https://docs.google.com/gview?embedded=true&url=' + 'www.ailinkgo.com/admin/excels/' + send_record.excel_path
             title = group_buy.title
             return Response(format_body(1, 'Success', {'excel_url': excel_url, 'title': title}))
 
