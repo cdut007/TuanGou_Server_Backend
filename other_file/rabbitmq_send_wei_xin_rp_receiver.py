d9883da5f1843f5a156cad6ad8be5caa3cb7aa66
Mike.zk <rock_or_bust@qq.com>
2017-12-23 17:57:47 +0800

    .

--- other_file/rabbitmq_send_wei_xin_rp_receiver.py
+++ other_file/rabbitmq_send_wei_xin_rp_receiver.py
@@ -1,35 +1,6 @@
 #!/usr/bin/env python
-import pika, json, time, urllib2, os
+import pika, json, time, urllib2
 from datetime import datetime
-
-class Config:
-    def __init__(self):
-        self.mode = os.environ.get('MODE', '')
-        self.cwd = os.getcwd()
-        self._init_()
-
-    def _init_(self):
-        folder_name = self.cwd.split('/')[-1]
-        if self.mode == 'TESTING'and folder_name == 'TuanGou_Server_Backend':
-            self.live_env()
-        elif self.mode == 'TESTING'and folder_name == 'TuanGou_Server_Backend_Testing':
-            self.staging_env()
-        elif self.mode == 'HOME' :
-            self.home_env()
-        else:
-            self.dev_env()
-
-    def live_env(self):
-        self.server_run_addr = '47.90.86.229:3000'
-
-    def staging_env(self):
-        self.server_run_addr = '47.90.86.229:3001'
-
-    def dev_env(self):
-        self.server_run_addr = '192.168.222.128:3000'
-
-    def home_env(self):
-        self.server_run_addr = '192.168.222.128:3000'
 
 
 class Consuming:
@@ -49,16 +20,16 @@ class Consuming:
 
     @staticmethod
     def http_send(group_buying_id, get_from):
-        config = Config()
-        url = 'http://' + config.server_run_addr + '/v2/api.rp.send'
+        url = 'http://47.90.86.229:3000/v2/api.rp.send'
         headers = {
             'Content-Type': 'application/json',
             'Authorization': 'eyJhbGciOiJIUzI1NiIsImV4cCI6MTU3MzcxODMxMywiaWF0IjoxNTEzMjM4MzEzfQ.eyJpZCI6Nzh9.QdDieApGFoFgLJkvRs-SgWlpFaGKR5ZjLDE6378AIOI'
         }
-        request = urllib2.Request(url=url, headers=headers, data=json.dumps({
+        data = json.dumps({
             'group_buying_id': group_buying_id,
             'get_from': get_from
-        }))
+        })
+        request = urllib2.Request(url=url, headers=headers, data=data)
         res = urllib2.urlopen(request)
         res = json.loads(res.read())
         print '{now}\t group_buying_id: {group_buying_id}\t get_from: {get_from}\t post_status: {post_status}\n'.format(
